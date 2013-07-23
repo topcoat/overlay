b4feba1e90e47c5d3594de2a60953a6e7d7ad695
piatra <andrei.br92@gmail.com>
2013-07-23 17:57:59 +0300

    update for npm

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,57 +1,68 @@
+/**
+*
+* Copyright 2012 Adobe Systems Inc.;
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
 /*global module:false*/
+
 module.exports = function(grunt) {
+
 
     // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        gruntfile: {
-            src: 'Gruntfile.js'
-        },
-
-        topcoat: {
-            download: {
-                options: {
-                    srcPath: 'tmp/src/',
-                    repos: '<%= pkg.topcoat %>'
-                }
-            }
-        },
-
-        unzip: {
-            controls: {
-                src: 'tmp/src/controls/*.zip',
-                dest: 'tmp/src/controls'
-            },
-            utils: {
-                src: 'tmp/src/utils/*.zip',
-                dest: 'tmp/src/utils'
-            }
-        },
 
         clean: {
-            tmp: ['tmp'],
-            zip: ['tmp/src/*.zip', 'tmp/src/controls/*.zip', 'tmp/src/skins/*.zip', 'tmp/src/utils/*.zip']
+            release: ['css'],
         },
 
-        compile: {
             stylus: {
+        compile: {
                 options: {
-                    import: ['utils', 'variables'],
+                    paths: ['src/', 'node_modules/topcoat-utils/src/mixins', 'node_modules/topcoat-overlay-base/src/mixins', 'node_modules/topcoat-theme/src', 'node_modules/topcoat-theme/src/includes'],
+                    import: ['overlay-mixin', 'utils', 'theme-topcoat-mobile-light', 'copyright.styl', 'global', 'fonts'],
                     compress: false
                 },
-                files: {
-                    'release/css/topcoat-overlay.css': ['src/copyright.styl', 'src/topcoat-overlay.styl']
-                }
+                files: [{
+                    src: 'src/topcoat-overlay.styl',
+                    dest: 'css/topcoat-overlay.css'
+                }]
             }
         },
 
         cssmin: {
             minify: {
                 expand: true,
-                cwd: 'release/css/',
+                cwd: 'css',
                 src: ['*.css', '!*.min.css'],
-                dest: 'release/css/',
-                ext: '.min.css'
+                dest: 'css',
+                ext: '.min.css',
+                options: {
+                    banner: grunt.file.read('src/copyright.styl').toString()
+                }
+            }
+        },
+
+        copy: {
+            release: {
+                files: [{
+                    expand: true,
+                    flatten: true,
+                    src: 'node_modules/topcoat-theme/font/**/*',
+                    dest: 'font'
+                }]
             }
         },
 
@@ -64,30 +75,38 @@ module.exports = function(grunt) {
                 ext: '.test.html'
             }
         },
-        nodeunit: {
-            tests: ['test/*.test.js']
+
+
+        simplemocha: {
+            options: {
+                ui: 'bdd',
+                reporter: 'Nyan'
+            },
+            all: {
+                src: ['test/*.test.js']
+            }
         },
+
         watch: {
-            files: 'src/*.styl',
-            tasks: ['build']
+            files: 'src/**/*.styl',
+            tasks: ['build', 'test']
         }
+
     });
 
     // These plugins provide necessary tasks.
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-stylus');
     grunt.loadNpmTasks('grunt-contrib-jade');
-    grunt.loadNpmTasks('grunt-contrib-nodeunit');
-    grunt.loadNpmTasks('grunt-topcoat');
-    grunt.loadNpmTasks('grunt-zip');
+    grunt.loadNpmTasks('grunt-simple-mocha');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-
-    grunt.loadTasks('tasks');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     // Default task.
-    grunt.registerTask('default', ['clean', 'topcoat', 'build']);
-    grunt.registerTask('build', ['compile', 'cssmin', 'jade', 'nodeunit', 'clean']);
-    grunt.registerTask('test', ['nodeunit']);
+    grunt.registerTask('default', ['clean', 'build', 'test', 'release']);
+    grunt.registerTask('build', ['stylus', 'jade']);
+    grunt.registerTask('test', ['simplemocha']);
+    grunt.registerTask('release', ['cssmin', 'copy']);
 
 };
